fd310c0aca22aa69580912758fd1f7493d03d533
Hussein Malkawi <r4w311a@gmail.com>
2022-07-05 21:02:18 +0300

    fix image error

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -54,7 +54,7 @@
                                             {{ $products->firstItem() + $loop->index }}</th>
                                         <td class="align-middle">{{ $product->product_name }}</td>
                                         <td class="align-middle"><img
-                                                src="{{Storage::disk('s3')->url('products/' . $ImageName)}}"
+                                                src="{{Storage::disk('s3')->url('products/' . $product->product_thumbnail)}}"
                                                 class="img-thumbnail mx-auto " style="width:80px;height:80px" />
                                         </td>
                                         <td class="align-middle">{{ $product->category->category_name }}</td>
