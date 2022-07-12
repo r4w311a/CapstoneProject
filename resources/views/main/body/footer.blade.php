a6b6ab7faa7a674734f1c1f17dd38eca827fc417
Hussein Malkawi <69742084+r4w311a@users.noreply.github.com>
2022-07-12 20:25:09 +0300

    edit footer menu

--- resources/views/main/body/footer.blade.php
+++ resources/views/main/body/footer.blade.php
@@ -1,15 +1,3 @@
 <footer class="footer">
-  <div class="row">
-    <div class="col d-flex">
-      <h4>INFORMATION</h4>
-      <a href="">About us</a>
-      <a href="">Contact Us</a>
-    </div>
-   
-    <div class="col d-flex">
-      <span><i class='bx bxl-facebook-square'></i></span>
-      <span><i class='bx bxl-twitter' ></i></span>
-    </div>
-  </div>
   <p class="copyright">Copyright &copy; 2022</p>
 </footer>
